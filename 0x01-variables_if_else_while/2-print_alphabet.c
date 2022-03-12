378941131710aa08d8141db5a836f017a85e7812
crondeluxe <crondeluxe4@gmail.com>
2022-03-12 09:38:08 -0800

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - this prints alphabets in lower case
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char d;
+
+	for (d = 'a'; d <= 'z'; d++)
+	{
+		putchar(d);
+		putchar('\n');
+	}
+	return (0);
+}
