d7b2ceb833face8ad80edacadbcd76e7f74bdd64
crondeluxe <crondeluxe4@gmail.com>
2022-03-18 09:45:20 +0000

    0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,10 @@
 #include "main.h"
 
-/* function declaration */
-void positive_or_negative(int i);
-
 /**
  * main - test function that prints if integer is positive or negative
- *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int i;
