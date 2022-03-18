4027c1e9db135ff9c6b3ac3dea952e7285cdfe75
crondeluxe <crondeluxe4@gmail.com>
2022-03-18 07:58:01 +0000

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - this sums two integers
+ * @a: number to be added
+ * @b: number to be added
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
