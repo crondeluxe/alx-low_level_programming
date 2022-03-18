86609035473977a31ed6e23cf2e932952f8f243a
crondeluxe <crondeluxe4@gmail.com>
2022-03-18 09:00:06 +0000

    0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,12 +4,16 @@
 void positive_or_negative(int i);
 
 /**
- * main - debugging the code
+ * main - test function that prints if integer is positive or negative
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+	int i;
+
+	i = 98;
 	positive_or_negative(0);
+
 	return (0);
 }
