6db8eb5f9d621a2e3798702d8198ce32a831f5c7
crondeluxe <crondeluxe4@gmail.com>
2022-03-19 04:47:35 +0000

    0

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -16,17 +16,27 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month >= 2 && day >= 60)
 		{
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 366 - day);
+			day++;
 		}
+		if (day > 60)
+		{
+			day--;
+			printf("Day of the year: %d\n", day);
+		}
+		printf("Remaining days: %d\n", 366 - day);
 	}
 	else if (year % 400 == 0 && year % 100 == 0)
 	{
 		if (month >= 2 && day >= 60)
 		{
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 366 - day);
+			day++;
 		}
+		if (day > 60)
+		{
+			day--;
+			printf("Day of the year: %d\n", day);
+		}
+		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
