278092cb9ba84b729cb189839df0d6567c19aef0
crondeluxe <crondeluxe4@gmail.com>
2022-03-20 23:36:21 +0000

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies input a and b
+ * @a: input
+ * @b: input
+ * Return: int
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
