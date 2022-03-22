4231e0493eb7568b5bc4004f3cda0332d88f74d1
crondeluxe <crondeluxe4@gmail.com>
2022-03-22 08:10:05 +0000

    1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - this swaps the value of two integers
+ * @a: a pointer to the integer
+ * @b: another pointer to the integer
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
