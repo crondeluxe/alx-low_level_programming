c531dba4cb4ac27f971fb1ab4b1a4c882db51faf
crondeluxe <crondeluxe4@gmail.com>
2022-03-23 04:39:35 +0000

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: pointer to the string
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int n, l, m;
+
+	l = 0;
+
+	while (str[l] != '\0')
+	{
+		l++;
+	}
+	if ((l % 2) == 0)
+	{
+		m = l / 2;
+
+		for (n = m; n < l; n++)
+		{
+			_putchar(str[n]);
+		}
+	}
+	else
+	{
+		m = (l - 1) / 2;
+
+		for (n = m + 1; n < l; n++)
+		{
+			_putchar(str[n]);
+		}
+	}
+	_putcar('\n');
+}
