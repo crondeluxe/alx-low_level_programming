cc8b447a23b4c6fc2faf0ff81d36be5b41a73cf1
crondeluxe <crondeluxe4@gmail.com>
2022-03-23 03:37:54 +0000

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - prints strings follow by new line
+ * @str: pointer to string
+ * Return: void
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+}
