6bdca48fffc732e3d01a211d562eedc80812fcb1
crondeluxe <crondeluxe4@gmail.com>
2022-03-24 03:45:30 +0000

    1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncat - this add dest strings to the src strings at n bytes
+ * @dest: pointer to the string destination
+ * @src: pointer to the source string
+ * @n: number of bytes
+ * Return: strings in destination
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int l, i;
+
+	l = 0;
+
+	while (dest[l] != '\0')
+	{
+		l++;
+	}
+	for (i = 0; src[i] != '\0'; i++, l++)
+	{
+		dest[l] = src[i];
+	}
+	dest[l] = '\0';
+	return (dest);
+}
